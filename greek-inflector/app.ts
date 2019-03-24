1907999221a6913edb1002cea6bca32bdc484874
Derek Davenport <derekdavenport@gmail.com>
2019-03-24 02:20:07 -0400

    nouns

---
+++ greek-inflector/app.ts
@@ -0,0 +1,338 @@
+﻿const normalized = {
+	'ή': 'ή',
+};
+
+type Declensions = {
+	[declension: string]: Endings;
+};
+
+type Endings = {
+	[ending: string]: Numbers;
+};
+
+type Numbers = {
+	singular: Cases;
+	plural: Cases;
+};
+
+type Cases = {
+	nominative: string;
+	genitive: string;
+	dative: string;
+	accusative: string;
+	vocative: string;
+};
+
+const nounDeclensions: Declensions = {
+	'1': {
+		'εα': {
+			'singular': {
+				nominative: 'εα',
+				genitive: 'εας',
+				dative: 'εᾳ',
+				accusative: 'εαν',
+				vocative: 'εα',
+			},
+			'plural': {
+				nominative: 'εαι',
+				genitive: 'εῶν',
+				dative: 'εαις',
+				accusative: 'εας',
+				vocative: 'εαι',
+			},
+		},
+		'ια': {
+			'singular': {
+				nominative: 'ια',
+				genitive: 'ιας',
+				dative: 'ιᾳ',
+				accusative: 'ιαν',
+				vocative: 'ια',
+			},
+			'plural': {
+				nominative: 'ιαι',
+				genitive: 'ιῶν',
+				dative: 'ιαις',
+				accusative: 'ιας',
+				vocative: 'ιαι',
+			},
+		},
+		'ρα': {
+			'singular': {
+				nominative: 'ρα',
+				genitive: 'ρας',
+				dative: 'ρᾳ',
+				accusative: 'ραν',
+				vocative: 'ρα',
+			},
+			'plural': {
+				nominative: 'ραι',
+				genitive: 'ρῶν',
+				dative: 'ραις',
+				accusative: 'ρας',
+				vocative: 'ραι',
+			},
+		},
+		'εά': {
+			'singular': {
+				nominative: 'εά',
+				genitive: 'εᾶς',
+				dative: 'εᾷ',
+				accusative: 'εάν',
+				vocative: 'εά',
+			},
+			'plural': {
+				nominative: 'εαί',
+				genitive: 'εῶν',
+				dative: 'εαῖς',
+				accusative: 'εάς',
+				vocative: 'εαί',
+			},
+		},
+		'ιά': {
+			'singular': {
+				nominative: 'ιά',
+				genitive: 'ιᾶς',
+				dative: 'ιᾷ',
+				accusative: 'ιάν',
+				vocative: 'ιά',
+			},
+			'plural': {
+				nominative: 'ιαί',
+				genitive: 'ιῶν',
+				dative: 'ιαῖς',
+				accusative: 'ιάς',
+				vocative: 'ιαί',
+			},
+		},
+		'ρά': {
+			'singular': {
+				nominative: 'ρά',
+				genitive: 'ρᾶς',
+				dative: 'ρᾷ',
+				accusative: 'ράν',
+				vocative: 'ρά',
+			},
+			'plural': {
+				nominative: 'ραί',
+				genitive: 'ρῶν',
+				dative: 'ραῖς',
+				accusative: 'ράς',
+				vocative: 'ραί',
+			},
+		},
+		'α': {
+			'singular': {
+				nominative: 'α',
+				genitive: 'ης',
+				dative: 'ῃ',
+				accusative: 'αν',
+				vocative: 'α',
+			},
+			'plural': {
+				nominative: 'αι',
+				genitive: 'ῶν',
+				dative: 'αις',
+				accusative: 'ας',
+				vocative: 'αι',
+			},
+		},
+		'ά': {
+			'singular': {
+				nominative: 'ά',
+				genitive: 'ῆς',
+				dative: 'ῇ',
+				accusative: 'άν',
+				vocative: 'ά',
+			},
+			'plural': {
+				nominative: 'αί',
+				genitive: 'ῶν',
+				dative: 'αῖς',
+				accusative: 'άς',
+				vocative: 'αί',
+			},
+		},
+		'η': {
+			'singular': {
+				nominative: 'η',
+				genitive: 'ης',
+				dative: 'ῃ',
+				accusative: 'ην',
+				vocative: 'η',
+			},
+			'plural': {
+				nominative: 'αι',
+				genitive: 'ων',
+				dative: 'αις',
+				accusative: 'ας',
+				vocative: 'αι',
+			},
+		},
+		'ή': {
+			'singular': {
+				nominative: 'ή',
+				genitive: 'ῆς',
+				dative: 'ῇ',
+				accusative: 'ήν',
+				vocative: 'ή',
+			},
+			'plural': {
+				nominative: 'αί',
+				genitive: 'ῶν',
+				dative: 'αῖς',
+				accusative: 'άς',
+				vocative: 'αί',
+			},
+		},
+		'ῆ': {
+			'singular': {
+				nominative: 'ῆ',
+				genitive: 'ῆς',
+				dative: 'ῇ',
+				accusative: 'ῆν',
+				vocative: 'ῆ',
+			},
+			'plural': {
+				nominative: 'αῖ',
+				genitive: 'ῶν',
+				dative: 'αῖς',
+				accusative: 'ᾶς',
+				vocative: 'αῖ',
+			},
+		},
+		'ης': {
+			'singular': {
+				nominative: 'ης',
+				genitive: 'ου',
+				dative: 'ῃ',
+				accusative: 'ην',
+				vocative: 'α',
+			},
+			'plural': {
+				nominative: 'αι',
+				genitive: 'ων',
+				dative: 'αις',
+				accusative: 'ας',
+				vocative: 'αι',
+			},
+		},
+		'ής': {
+			'singular': {
+				nominative: 'ής',
+				genitive: 'οῦ',
+				dative: 'ῇ',
+				accusative: 'ήν',
+				vocative: 'α',
+			},
+			'plural': {
+				nominative: 'αί',
+				genitive: 'ῶν',
+				dative: 'αῖς',
+				accusative: 'άς',
+				vocative: 'αί',
+			},
+		},
+	},
+	'2': {
+		'ος': {
+			'singular': {
+				nominative: 'ος',
+				genitive: 'ου',
+				dative: 'ͅω',
+				accusative: 'ον',
+				vocative: 'ε',
+			},
+			'plural': {
+				nominative: 'οι',
+				genitive: 'ων',
+				dative: 'οις',
+				accusative: 'ους',
+				vocative: 'οι',
+			},
+		},
+		'ός': {
+			'singular': {
+				nominative: 'ός',
+				genitive: 'οῦ',
+				dative: 'ῷ',
+				accusative: 'όν',
+				vocative: 'ε',
+			},
+			'plural': {
+				nominative: 'οί',
+				genitive: 'ῶν',
+				dative: 'οῖς',
+				accusative: 'ούς',
+				vocative: 'οί',
+			},
+		},
+		'ον': {
+			'singular': {
+				nominative: 'ον',
+				genitive: 'ου',
+				dative: 'ͅω',
+				accusative: 'ον',
+				vocative: 'ον',
+			},
+			'plural': {
+				nominative: 'α',
+				genitive: 'ων',
+				dative: 'οις',
+				accusative: 'α',
+				vocative: 'α',
+			},
+		},
+		'όν': {
+			'singular': {
+				nominative: 'όν',
+				genitive: 'οῦ',
+				dative: 'ῷ',
+				accusative: 'όν',
+				vocative: 'όν',
+			},
+			'plural': {
+				nominative: 'ά',
+				genitive: 'ῶν',
+				dative: 'οῖς',
+				accusative: 'ά',
+				vocative: 'ά',
+			},
+		},
+	},
+};
+
+const accents = [
+	"́",
+];
+
+function normalize(text: string): string {
+	for (const [bad, good] of Object.entries(normalized)) {
+		text = text.split(bad).join(good);
+	}
+	return text;
+}
+
+function inflectNoun(lexicalForm: string, declension: '1' | '2', num: 'singular' | 'plural', cas: 'nominative' | 'genitive' | 'dative' | 'accusative' | 'vocative') {
+	// lexicalForm = normalize(lexicalForm);
+	const endings = nounDeclensions[declension];
+	const ending = Object.keys(endings).find(ending => lexicalForm.endsWith(ending));
+	if (!ending) {
+		throw Error(lexicalForm + ' has an unknown ending for declension ' + declension);
+	}
+	const stem = lexicalForm.substring(0, lexicalForm.length - ending.length)
+	const number = endings[ending];
+
+	return stem + number[num][cas];
+}
+
+console.log(inflectNoun('δόξα', '1', 'singular', 'genitive'));
+console.log(inflectNoun('τιμή', '1', 'singular', 'genitive'));
+console.log(inflectNoun('φωνή', '1', 'singular', 'genitive'));
+console.log(inflectNoun('χαρά', '1', 'singular', 'genitive'));
+console.log(inflectNoun('γλῶττα', '1', 'singular', 'genitive'));
+console.log(inflectNoun('γῆ', '1', 'singular', 'genitive'));
+console.log(inflectNoun('κύριος', '2', 'singular', 'genitive'));
+console.log(inflectNoun('τέκνον', '2', 'singular', 'genitive'));
+console.log(inflectNoun('ἱερόν', '2', 'singular', 'genitive'));
+console.log(inflectNoun('ναός', '2', 'singular', 'genitive')); 
